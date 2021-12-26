8e9fc4b04bde03eaa94c23efdc26d85f6b11043b
ヒルミーサクティ <abandel09@gmail.com>
2021-12-26 17:30:27 +0700

    Update apikey.js

--- assets/js/plugins/apikey.js
+++ assets/js/plugins/apikey.js
@@ -27,7 +27,7 @@
           } else {
             textnya.textContent = '[ INFO ] Apikey anda tidak valid!'
             swal("Apikey Tidak Valid!", "", "error");
-            alert('Apikey Tidak Valid. Silahkan beli apikey ke Instagram @zeeoneofc');
+            alert('Apikey Tidak Valid. Silahkan beli apikey!');
             amlert.classList.remove('hideop')
           }
         };
